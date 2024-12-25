8597d4572a9dc1ca75e6e587bfb4ba50570f0c52
specificlanguage <specificlanguage@gmail.com>
2024-12-25 22:56:21 +0800

    day24 finish
    brute forcing this one because while I know how a full adder works, I couldn't be pissed to actually remember how to deconstruct it for this problem. Absolute kudos to this thread for solving it: https://www.reddit.com/r/adventofcode/comments/1hla5ql/2024_day_24_part_2_a_guide_on_the_idea_behind_the/

--- 2024/Day24/day24_part2.py
+++ 2024/Day24/day24_part2.py
@@ -3,13 +3,12 @@ import queue
 from collections import defaultdict, deque, Counter
 from functools import cache
 import itertools
+from keyword import kwlist
 from math import log10, floor, isclose, lcm
 import re
 
 FILEPATH = 'day24input'
-init_bools = {}
-equations = {}
-lineage = {}
+init_bools, equations, lineage = {}, {}, {}
 
 with open(FILEPATH) as f:
     vals, eqs = f.read().split("\n\n")
@@ -19,10 +18,14 @@ with open(FILEPATH) as f:
     for equation in eqs.split("\n"):
         k1, op, k2, output = re.match(r"(.{3}) (XOR|OR|AND) (.{3}) -> (.{3})", equation).groups()
         equations[(k1, op, k2)] = output
-        lineage[output] = [k1, k2]
+        lineage[output] = [k1, k2, op]
+
+# Convert to binary number
+def toBinary(letter, bools):
+    return "".join(reversed([str(k[1]) for k in sorted([(k,v) for k,v in bools.items() if k[0] == letter])]))
 
 # Try to answer as you can. O(n^2) iteration here, but whatever.
-def calculate():
+def calculate(equations):
     to_eval = deque(equations.items())
     bools = init_bools.copy()
 
@@ -38,63 +41,62 @@ def calculate():
                 bools[output] = bools[k1] ^ bools[k2]
         else:
             to_eval.append((key, output))
-    return retrieveBinaryNumber("z", bools)
+    return toBinary("z", bools)
 
-def retrieveBinaryNumber(letter, bools):
-    return int("".join(reversed([str(k[1]) for k in sorted([(k,v) for k,v in bools.items() if k[0] == letter])])), 2)
+x = toBinary("x", init_bools)
+y = toBinary("y", init_bools)
+z = calculate(equations)
+print(x, y, z)
 
-def findIncorrectBits(a, b):
-    bits = []
-    a, b = f'{a:b}', f'{b:b}'
-    for i in range(len(a)): # Assuming it is the same length as b
-        if a[i] != b[i]:
-            bits.append(len(a) - i - 1)
-    return bits
+faulty_ops = []
+and_gates = set(output for output in lineage if lineage[output][2] == "AND" and lineage[output][1][0] in ["x", "y"] and lineage[output][0][0] in ["x", "y"])
+xor_gates = set(output for output in lineage if lineage[output][2] == "XOR" and lineage[output][1][0] in ["x", "y"] and lineage[output][0][0] in ["x", "y"])
+for output in lineage:
+    k1, k2, op = lineage[output]
+    if output[0] == "z":
+        if op != "XOR":
+            faulty_ops.append(output)
+    if output[0] != "z" and k1[0] not in ["x", "y"] and k2[0] not in ["x", "y"]:
+        if op == "XOR":
+            faulty_ops.append(output)
+    if op == "OR":
+        and_gates.discard(k1)
+        and_gates.discard(k2)
+    if op == "XOR":
+        xor_gates.discard(k1)
+        xor_gates.discard(k2)
 
-def findLineage(bit_number):
-    ret = set()
-    queue = deque([f"z{bit_number:02}"])
-    while queue:
-        key = queue.popleft()
-        ret.add(key)
-        if key in lineage:
-            for desc in lineage[key]:
-                queue.append(desc)
-    return ret
+print(faulty_ops)
+print(and_gates)
+print(xor_gates)
+print(",".join(sorted(['frt', 'z11', 'z23', 'z05', 'sps', 'tst', 'cgh', 'pmd'])))
 
-def genCandidatesToSwap(z):
+# def diagnoseLineage(bit, level=0):
+#     if bit[0] == "y" or bit[0] == "x":
+#         print("  " * level + f"{bit} : {bools[bit]}")
+#         return
+#     k1, k2, op = lineage[bit]
+#     v1, v2, vb = bools[k1], bools[k2], bools[bit]
+#     print("  " * level + f"{k1} {op} {k2} = {bit} : ({v1} {op} {v2} = {vb})")
+#     diagnoseLineage(k1, level + 1)
+#     diagnoseLineage(k2, level + 1)
+#
+# def verify(x, y, z):
+#     x = x[::-1]
+#     y = y[::-1]
+#     z = z[::-1]
+#     carry = 0
+#     for i in range(len(x)):
+#         add = int(x[i]) + int(y[i]) + carry
+#         res, next_carry = add % 2, add // 2
+#         if res != int(z[i]):
+#             print("incorrect bit", i, f"z{i:02}: {x[i]}, y{i:02}: {y[i]}, z{i:02}: {z[i]}, carry: {carry}")
+#             diagnoseLineage(bit=f"z{i:02}")
+#             print("----------------------")
+#         carry = next_carry
 
-    def uniqueItems(x): # Need to check if all items within the combo list are unique.
-        seen = set()
-        for t in x:
-            for n in t:
-                if n in seen: return False
-                seen.add(n)
-        return True
+# verify(x, y, z)
+# for i in range(6):
+#     diagnoseLineage(bit=f"z{i:02}")
 
-    # Convert to binary number
-    x = retrieveBinaryNumber("x", init_bools)
-    y = retrieveBinaryNumber("y", init_bools)
-    ibs = findIncorrectBits(x + y, z)
-    candidates = []
-    for bs in ibs:
-        suspects = findLineage(bs)
-        for sus in suspects:
-            if not re.match(r"[x|y]\d{2}", sus):
-                candidates.append(sus)
-
-    # Generate all pairwise swaps
-    swaps = itertools.combinations(candidates, 2)
-
-    # Generate all possible four-sets
-    four_sets = itertools.combinations(swaps, 4)
-    return filter(uniqueItems, four_sets)
-
-z = calculate()
-swap_possibilities = genCandidatesToSwap(z)
-for x in swap_possibilities:
-    print(x)
-
-
-
-
+# z05,
